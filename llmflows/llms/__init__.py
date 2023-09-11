cd4327c080f3ef88d7471ed41c6949127f74baa5
Stoyan Stoyanov <14061867+stoyan-stoyanov@users.noreply.github.com>
2023-09-10 20:22:53 -0700

    implemented a client for anthropics claude API [wip]

--- llmflows/llms/__init__.py
+++ llmflows/llms/__init__.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-module-docstring
 from .openai import OpenAI
 from .openai_chat import OpenAIChat
+from .claude_chat import ClaudeChat
 from .openai_embeddings import OpenAIEmbeddings
 from .message_history import MessageHistory
