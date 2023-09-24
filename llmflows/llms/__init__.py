148bcab0643a6062731ff0a40d554f39db66a576
Stoyan Stoyanov <14061867+stoyan-stoyanov@users.noreply.github.com>
2023-09-24 16:45:28 -0700

    removing azure classes from __init__

--- llmflows/llms/__init__.py
+++ llmflows/llms/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-module-docstring
+from .openai import OpenAI
 from .openai_chat import OpenAIChat
-from .azure_openai_chat import AzureOpenAIChat
 from .claude_chat import ClaudeChat
 from .openai_embeddings import OpenAIEmbeddings
 from .message_history import MessageHistory
